c0e80f6e1d5b57f7967c93b0a51ac390daa1c14a
Prashanthkv9871 <kvprashanth987@gmail.com>
2026-04-15 23:11:54 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center font-bold  text-4xl text-green-600">Aforro React Assignment</div>
+    </>
+  )
+}
+
+export default App
